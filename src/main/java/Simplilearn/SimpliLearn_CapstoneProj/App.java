7ee0c01393e4ffbd53cfe5a08b9e5bd62243ce1a
shivanijamadar <jamadarshivani25@gmail.com>
2023-04-01 13:10:59 +0530

    CapstoneProject

---
+++ src/main/java/Simplilearn/SimpliLearn_CapstoneProj/App.java
@@ -0,0 +1,7 @@
+package Simplilearn.SimpliLearn_CapstoneProj;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/JDBCConnection/DataPageManager.java
@@ -0,0 +1,24 @@
+package JDBCConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Objects;
+
+import org.testng.annotations.Test;
+
+import API.API_GetCategory;
+
+public class DataPageManager {
+	
+	private Databaseconnect databaseconnect;
+
+    public Databaseconnect d_getDatabaseconnect()
+    {
+         return(Objects.isNull(databaseconnect))? databaseconnect= new Databaseconnect():databaseconnect;
+    }
+
+	
+	
+}
